74450dc134171ff7365d9809618af472a04a0ecd
vladho <vladhopilo@gmail.com>
2020-12-22 12:54:24 +0200

    done

--- js/task-2.js
+++ js/task-2.js
@@ -13,16 +13,16 @@ const inventory = {
 
 const invokeInventoryAction = function (itemName, action) {
   console.log(`Invoking action on ${itemName}`);
-  action.bind(inventory)(itemName);
+  action( itemName);
 };
 
-invokeInventoryAction('Medkit', inventory.add);
+invokeInventoryAction('Medkit', inventory.add.bind(inventory));
 // Invoking action on Medkit
 // Adding Medkit to inventory
 
 console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
 
-invokeInventoryAction('Gas mask', inventory.remove);
+invokeInventoryAction('Gas mask', inventory.remove.bind(inventory));
 // Invoking action on Gas mask
 // Removing Gas mask from inventory
 
